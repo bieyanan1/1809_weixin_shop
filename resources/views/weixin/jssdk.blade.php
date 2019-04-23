316d4b19c72d154b075ebb00346205bb56df6d0f
bieyanan1 <13716557807@163.com>
2019-04-23 13:02:34 +0000

    debug

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -19,7 +19,7 @@
     <script>
         wx.ready(function(){
             $("#btn1").click(function(){
-                
+
                 wx.chooseImage({
                     count: 3, // 默认9
                     sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
@@ -27,26 +27,16 @@
                     success: function ( res) {
                         var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                         var img = "";
+
                         $.each(localIds,function(i,v){
                             img += v+',';
                             var node = "#imgs"+i;
                             $(node).attr('src',v);
-                            
-
-
-                            wx.uploadImage({
-                                localId: v, // 需要上传的图片的本地ID，由chooseImage接口获得
-                                isShowProgressTips: 1, // 默认为1，显示进度提示
-                                success: function (res1) {
-                                    var serverId = res1.serverId; // 返回图片的服务器端ID
-                                    console.log(res1);
-                                }
-                            });
                         })
 
 
                         $.ajax({
-                            url : '/js/getImg?img='+img,     //将上传的照片id发送给后端
+                            url : '/js/getImg?img='+img,     
                             type: 'get',
                             success:function(d){
                                 console.log(d);
