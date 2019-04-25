d0020fa101527980ba1f629733261cb02a180c58
bieyanan1 <13716557807@163.com>
2019-04-25 01:18:21 +0000

    检测

--- app/Http/Controllers/weixin/WxController.php
+++ app/Http/Controllers/weixin/WxController.php
@@ -24,7 +24,6 @@ class WxController extends Controller
         }else{
 
             $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APP_ID').'&secret='.env('WX_APP_SEC');
-            // echo $url;die;
             $json_str = file_get_contents($url);
             $arr = json_decode($json_str,true);
             Redis::set($redis_key,$arr['access_token']);
@@ -36,12 +35,10 @@ class WxController extends Controller
 
     public function getU()
     {
-        $a = $_GET['code'];
-        $code = getAccessToken($a);
+        $code = $_GET['code'];
         $token = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APP_ID').'&secret='.env('WX_APP_SEC').'&code='.$code.'&grant_type=authorization_code';
         $response = json_decode(file_get_contents($token),true);
         $access_token = $response['access_token'];
-        print_r($access_token);die;
         $openid = $response['openid'];
         $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $res = json_decode(file_get_contents($url),true);
